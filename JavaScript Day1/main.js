945287f4593a47a0e87659f2c98ba24223f6cd28
NoureddineMa <noureddinemaher13@gmail.com>
2022-07-11 14:00:46 +0200

    Day 1 Completed

--- JavaScript Day1/main.js
+++ JavaScript Day1/main.js
@@ -110,6 +110,82 @@ const isCompleted = Maher.filter(function(maher){
 console.log(isCompleted);
 
 
+// comparaiton :
+const l = 11;
+// == value 
+// === Type and Value 
+if(x == 10){
+    console.log('x is equal to 10');
+} else if ( x > 10) {
+    console.log('x is greater than 10');
+} else {
+    console.log('x is less than 10');
+}
+// OR  || and && 
+
+// OR  ||
+const m= 4;
+const y= 11;
+
+if(m > 5 || y > 12){
+    console.log('x is greater than 5 or y is greater than 12');
+}else{
+    console.log('x is less than 5 or y is less than 12');
+}
+
+// AND &&
+
+if(m == 4 || y == 11){
+    console.log('m is equal to 4 and y is equal to 11')
+    }else {
+        console.log('m is not equal to 4 and y is not equal to 11');
+    }
+
+ // shorthand of if statement :
+ const n = 5;
+ 
+ const color = n > 4 ? 'red' : 'blue';
+//  let color = 'green';
+//  console.log(color);
+
+
+//  Switch case :
+switch(color){
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+        default:
+            console.log('color is not red or blue');
+            break;
+}
+
+// Functions :
+function addNums(num1,num2){
+   return num1+num2;
+}
+console.log(addNums(3,6));
+
+// Arrow Functions :
+const TwoNumbers = (N1=1 , N2=2) => N1 + N2;
+console.log(TwoNumbers(3,7));
+
+// Arrow functions whit 1 parametre
+const TwoN = N1 => N1 +5;
+console.log(TwoN(3));
+
+ 
+
+
+
+
+
+
+
+
+
 
 
 
